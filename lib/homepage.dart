a93eba9ecfea44bc7f0a1585eea1f8207843750d
CHRIS-1023 <omondichris9@gmail.com>
2023-07-21 10:47:46 +0300

    flutter bloc practice

---
+++ lib/homepage.dart
@@ -0,0 +1,71 @@
+import 'package:bloc_practice/counter_bloc.dart';
+import 'package:bloc_practice/counter_event.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _bloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+          child: StreamBuilder(
+        stream: _bloc.counter,
+        initialData: 0,
+        builder: (context, AsyncSnapshot<int> snapshot) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '${snapshot.data}',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          );
+        },
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventSink.add(IncrementEvent()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+}
